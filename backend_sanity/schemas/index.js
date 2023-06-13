bd1ea3d4ebf98998d89470e2b71e852b7224a9e3
johnliu-code <79877649+johnliu-code@users.noreply.github.com>
2023-06-13 15:23:46 -0400

    1. Backend with Sanity frontend with react 2. Sass for the UI 3. Images from source folder

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import brands from "./brands";
+import workExperience from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [
+    testimonials,
+    abouts,
+    contact,
+    experiences,
+    skills,
+    brands,
+    workExperience,
+    works
+]
